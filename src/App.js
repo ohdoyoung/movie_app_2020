e35b504643905dc7765ee79c820e1002f14f529f
ohdoyoung <dhehdud456@naver.com>
2021-01-22 21:11:20 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "kimchi",
+    image: "https://blog.naver.com/bongs1021/221140403623",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "samgyeopsal",
+    image:
+      "https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=%EC%82%BC%EA%B2%B9%EC%82%B4&oquery=%EA%B9%80%EC%B9%98&tqi=hsqFjsprvhGss6VlOWNssssstPs-014284#",
+    rating: 4.2,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
