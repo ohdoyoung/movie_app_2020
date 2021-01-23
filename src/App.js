a7bb87ff6f37ef21cb1ffc333d9d9c986cd03bf3
ohdoyoung <dhehdud456@naver.com>
2021-01-23 21:59:26 +0900

    error->movie.css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import React from "react";
-import PropTypes from "prop-types";
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image: "https://blog.naver.com/bongs1021/221140403623",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "samgyeopsal",
-    image:
-      "https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=%EC%82%BC%EA%B2%B9%EC%82%B4&oquery=%EA%B9%80%EC%B9%98&tqi=hsqFjsprvhGss6VlOWNssssstPs-014284#",
-    rating: 4.2,
-  },
-];
+import Home from "./routes/Home";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
